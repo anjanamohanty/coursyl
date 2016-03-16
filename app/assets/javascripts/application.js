f83473a96d3751221f5dff7018c2d4eae64eaca9
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-03-16 00:04:19 -0400

    Add code for smooth-scroll, get it working for students link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,3 +44,86 @@ function deleteRow(number) {
 // Back on the course detail page, you'll notice that clicking on the various nav buttons in the blue box will cause the page to snap to that section.
 // Implement smooth scrolling instead. You'll have to look it up on Google, and I expect that you'll copy and paste a solution into your code.
 // It's okay if you don't understand it all; welcome to the vast world of JavaScript.
+
+
+/**
+    by Nemes Ioan Sorin - not an jQuery big fan
+    therefore this script is for those who love the old clean coding style
+    @id = the id of the element who need to bring  into view
+
+    Note : this demo scrolls about 12.700 pixels from Link1 to Link3
+*/
+(function()
+{
+      window.setTimeout = window.setTimeout; //
+})();
+
+      var smoothScr = {
+      iterr : 30, // set timeout miliseconds ..decreased with 1ms for each iteration
+        tm : null, //timeout local variable
+      stopShow: function()
+      {
+        clearTimeout(this.tm); // stopp the timeout
+        this.iterr = 30; // reset milisec iterator to original value
+      },
+      getRealTop : function (el) // helper function instead of jQuery
+      {
+        var elm = el;
+        var realTop = 0;
+        do
+        {
+          realTop += elm.offsetTop;
+          elm = elm.offsetParent;
+        }
+        while(elm);
+        return realTop;
+      },
+      getPageScroll : function()  // helper function instead of jQuery
+      {
+        var pgYoff = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+        return pgYoff;
+      },
+      anim : function (id) // the main func
+      {
+        this.stopShow(); // for click on another button or link
+        var eOff, pOff, tOff, scrVal, pos, dir, step;
+
+        eOff = document.getElementById(id).offsetTop; // element offsetTop
+
+        tOff =  this.getRealTop(document.getElementById(id).parentNode); // terminus point
+
+        pOff = this.getPageScroll(); // page offsetTop
+
+        if (pOff === null || isNaN(pOff) || pOff === 'undefined') pOff = 0;
+
+        scrVal = eOff - pOff; // actual scroll value;
+
+        if (scrVal > tOff)
+        {
+          pos = (eOff - tOff - pOff);
+          dir = 1;
+        }
+        if (scrVal < tOff)
+        {
+          pos = (pOff + tOff) - eOff;
+          dir = -1;
+        }
+        if(scrVal !== tOff)
+        {
+          step = ~~((pos / 4) +1) * dir;
+
+          if(this.iterr > 1) this.iterr -= 1;
+          else this.itter = 0; // decrease the timeout timer value but not below 0
+          window.scrollBy(0, step);
+          this.tm = window.setTimeout(function()
+          {
+             smoothScr.anim(id);
+          }, this.iterr);
+        }
+        if(scrVal === tOff)
+        {
+          this.stopShow(); // reset function values
+          return;
+        }
+    }
+ }
